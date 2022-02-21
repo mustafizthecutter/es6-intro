e59c335e11a5781119913128ff009e06f5f1cfe5
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-21 23:11:06 +0600

    committing agian

--- maap.js
+++ maap.js
@@ -1,5 +1,5 @@
-const numbers = [4, 6, 8, 10];
-const output2 = [];
+// const numbers = [4, 6, 8, 10];
+// const output2 = [];
 // function doubleOld(number) {
 //     return number * 2;
 // }
@@ -9,9 +9,24 @@ const output2 = [];
 //     output2.push(result);
 // }
 // console.log(output2);
-const output = numbers.map(x => x * 2);
-const square = numbers.map(x => x * x);
-console.log(square);
+// const output = numbers.map(x => x * 2);
+// const square = numbers.map(x => x * x);
+// console.log(square);
 // const output = numbers.map(number => number * 2);
 // const output = numbers.map(doubleIt);
 // console.log(output);
+// const friends = ['chiku', 'fardina', 'hult prize', 'tom lobbiatedruion'];
+// const friendLength = friends.map(friend => friend.length);
+// console.log(friendLength);
+// const robinGayst = ['pushti', 'misthi', 'jukayotonor', 'dushtanumberOne'];
+// const robinGaystNew = robinGayst.map(robin => robin.length);
+// console.log(robinGaysNew);
+const friends = [
+    { name: 'mustafiz', age: 34, profes: 'bekar', bari: 'sylhet' },
+    { name: 'murad', age: 45, profes: 'bekar', bari: 'noakhali' },
+    { name: 'sojib', age: 3478, profes: 'jushta', bari: 'sopno' },
+    { name: 'robin', age: 3904, profes: 'bekar', bari: 'moon' }
+];
+friends.forEach(friend => console.log(friend));
+// const chiku = friends.map(friend => friend.bari);
+// console.log(chiku);
